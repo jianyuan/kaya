e4b5c4c6b8b794210037c4575e4bce7df8753c8b
Jian Yuan Lee <jianyuan@gmail.com>
2023-01-10 23:31:37 +0000

    Improve API

--- examples/http.ts
+++ examples/http.ts
@@ -1,12 +1,17 @@
+import { z } from "zod";
 import { Kaya } from "../src/index.js";
 
 const app = new Kaya()
-  .get("/", () => {
-    return new Response("OK");
-  })
-  .get("/hello/:name", () => {
-    return new Response("hello world!");
-  });
+  .get("/", (ctx) => ctx.text("Hello world!"))
+  .get(
+    "/hello",
+    {
+      querySchema: z.object({
+        name: z.string().default("world"),
+      }),
+    },
+    (ctx) => ctx.text(`Hello ${ctx.query.name}!`)
+  );
 
 console.log("Hello via Bun!");
 app.printRoutes();
--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,75 @@ import Router, {
   type HTTPMethod,
   type Handler as FMWHandler,
 } from "find-my-way";
+import { ZodTypeAny } from "zod";
 
-type Handler = (request: Request) => Response | Promise<Response>;
+export type Parser = ZodTypeAny;
+
+export class Context {
+  request: Request;
+  query?: Record<string, any>;
+
+  constructor(request: Request, { query }: { query?: Record<string, any> }) {
+    this.request = request;
+    this.query = query;
+  }
+
+  text(text: string, options?: ResponseInit) {
+    return new Response(text, options);
+  }
+}
+
+type InternalHandler = (
+  request: Request,
+  params: Record<string, string | undefined>,
+  searchParams: Record<string, string | string[]>
+) => Response | Promise<Response>;
+type Handler = (context: Context) => Response | Promise<Response>;
+type RouteOptions = {
+  querySchema?: Parser;
+};
+
+type RouteMethod =
+  | ((path: string, options: RouteOptions, handler: Handler) => Kaya)
+  | ((path: string, handler: Handler) => Kaya);
 
 export class Kaya {
   #router = Router();
   constructor() {}
 
-  get(path: string, handler: Handler) {
-    this.#router.get(path, handler as FMWHandler<any>);
+  get: RouteMethod = (
+    path: string,
+    options: RouteOptions | Handler,
+    handler?: Handler
+  ) => {
+    if (handler === undefined) {
+      handler = options as Handler;
+      options = {};
+    }
+
+    if (handler === undefined) {
+      throw new Error("undefined handler");
+    }
+
+    const internalHandler: InternalHandler = (
+      request,
+      params,
+      searchParams
+    ) => {
+      console.log("params", params);
+      console.log("searchParams", searchParams);
+
+      const opts = options as RouteOptions;
+      let query;
+      if (opts?.querySchema) {
+        query = opts.querySchema.parse(searchParams);
+      }
+      const context = new Context(request, { query });
+      return (handler as Handler)(context);
+    };
+    this.#router.get(path, internalHandler as FMWHandler<any>);
     return this;
-  }
+  };
 
   notFoundHandler() {
     return new Response("404 Not Found", { status: 404 });
@@ -27,7 +85,11 @@ export class Kaya {
       return this.notFoundHandler();
     }
 
-    return (result.handler as unknown as Handler)(request);
+    return (result.handler as unknown as InternalHandler)(
+      request,
+      result.params,
+      result.searchParams
+    );
   };
 
   printRoutes() {
